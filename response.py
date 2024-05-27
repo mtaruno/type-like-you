1a2b8911e1e578f4f2770a62e0c45716858526db
mtaruno <matthew.taruno@gmail.com>
2024-05-27 17:28:20 +0800

    formatted get_response to be more front end friendly

--- response.py
+++ response.py
@@ -3,33 +3,16 @@ from utils.api import query_gpt4
 import argparse
 
 
-def get_response(message):
-    # get prompt
-    with open("prompts/imitate.txt", "r") as f:
-        prompt = f.read()
+def get_response(system_response, user_message):
 
-    # get conversation to add to prompt
-    conversation = get_conversation(lines=50, person_path="data/bryan.txt")
-    # adding user message 
-    formatted_prompt = prompt.format(conversation=conversation, message=message)
-    print(formatted_prompt)
-    print(f"Tokens: {len(formatted_prompt.split())}") # gpt4o max context length is 128k
+    messages = []
 
-    messages = [
-                {
-                    "role": "system",
-                    "content": formatted_prompt,
-                }, 
-                {
-                    "role": "user",
-                    "content": message,
-                }
-            ]
+    # add system content
+    messages.append(system_response)
+    # add user content
+    messages.append(user_message)
+    
     return query_gpt4(messages=messages, model="gpt-4o", max_tokens=150).content
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -38,6 +21,27 @@ if __name__ == "__main__":
     parser.add_argument('--message', '-m', type=str, required=True, help='The message content to process')
     args = parser.parse_args()
     message = args.message
-    response = get_response(message)
+
+    # get prompt
+    with open("prompts/imitate.txt", "r") as f:
+        prompt = f.read()
+    
+    # get conversation to add to prompt
+    conversation = get_conversation(lines=50, person_path="data/bryan.txt")
+    
+    formatted_prompt = prompt.format(conversation=conversation, message=message)
+    print(formatted_prompt)
+
+    print(f"Tokens: {len(formatted_prompt.split())}") # gpt4o max context length is 128k
+
+    sys_prompt = {
+                    "role": "system",
+                    "content": formatted_prompt}
+    message_obj = {
+                    "role": "user",
+                    "content": message,
+                }
+   
+    response = get_response(sys_prompt,message_obj)
     print(response)
 
